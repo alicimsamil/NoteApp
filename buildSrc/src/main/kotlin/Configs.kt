257ce86af73046d53aed6dfb93e3e150d8c15be8
Ali Şamil Küçük <ali.kucuk@appcent.mobi>
2023-05-05 14:13:04 +0300

    create and define buildSrc

---
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -0,0 +1,10 @@
+object Configs {
+    const val compileSdkVersion  = 33
+    const val minSdkVersion  = 21
+    const val targetSdkVersion  = 33
+    const val buildToolsVersion = "30.0.3"
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+    const val applicationId = "com.task.noteapp"
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,12 @@
+object Versions{
+    const val androidApplicationPathVersion = "7.2.2"
+    const val androidLibraryPathVersion = "7.2.2"
+    const val kotlinGradleVersion = "1.7.10"
+    const val coreLibVersion = "1.9.0"
+    const val appcompatLibVersion = "1.6.1"
+    const val materialLibVersion = "1.8.0"
+    const val constraintLayoutVersion = "2.1.4"
+    const val testJunitVersion = "4.13.2"
+    const val testExtJunitVersion = "1.1.5"
+    const val testEspressoVersion = "3.5.1"
+}
