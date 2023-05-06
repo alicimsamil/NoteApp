a03477c385b511009d056e9df21c7ef217e29a1d
Ali Şamil Küçük <alicimsamil@gmail.com>
2023-05-06 16:43:08 +0300

    add test database

---
+++ app/src/androidTest/java/com/task/noteapp/di/TestDatabaseModule.kt
@@ -0,0 +1,22 @@
+package com.task.noteapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.task.noteapp.data.local.NoteDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule {
+    @Provides
+    @Named("TestDatabase")
+    fun injectInMemoryRoom(@ApplicationContext context: Context) =
+        Room.inMemoryDatabaseBuilder(context, NoteDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+}
