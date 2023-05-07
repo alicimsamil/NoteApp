912f4f97bc977e34bf92adaeda80551250e8c399
Ali Şamil Küçük <alicimsamil@gmail.com>
2023-05-07 10:03:46 +0300

    add new test to add note usecase

--- app/src/test/java/com/task/noteapp/domain/AddNoteUseCaseTest.kt
+++ app/src/test/java/com/task/noteapp/domain/AddNoteUseCaseTest.kt
@@ -1,5 +1,6 @@
 package com.task.noteapp.domain
 
+import com.task.noteapp.core.data.Error
 import com.task.noteapp.core.data.Success
 import com.task.noteapp.data.model.NoteModel
 import com.task.noteapp.data.repository.FakeNoteRepositoryImpl
@@ -34,6 +35,21 @@ class AddNoteUseCaseTest {
         val result = useCase.invoke(note)
         assertEquals(Success(Unit), result)
         assertTrue(repository.notes.contains(note))
+    }
+
+
+    @Test
+    fun add_note_with_empty_title_should_be_fail() = runTest {
+        val note = NoteModel(
+            imageUrl = "www.test.com",
+            date = 214513552125,
+            noteTitle = "",
+            note = "Note",
+            editedTag = false,
+            id = 1
+        )
+        val result = useCase.invoke(note)
+        assertEquals(Error("Please check title and note areas."), result)
     }
 
 
