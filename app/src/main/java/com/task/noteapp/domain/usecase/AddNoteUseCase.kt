2983631c4f7585778cea4b1aab47745f030e676e
Ali Şamil Küçük <alicimsamil@gmail.com>
2023-05-07 09:52:12 +0300

    add logic to add note use case

--- app/src/main/java/com/task/noteapp/domain/usecase/AddNoteUseCase.kt
+++ app/src/main/java/com/task/noteapp/domain/usecase/AddNoteUseCase.kt
@@ -1,11 +1,18 @@
 package com.task.noteapp.domain.usecase
 
+import com.task.noteapp.core.data.Error
 import com.task.noteapp.data.model.NoteModel
 import com.task.noteapp.domain.repository.local.NoteRepository
 import javax.inject.Inject
 
 class AddNoteUseCase @Inject constructor(private val repository: NoteRepository) {
 
-    suspend operator fun invoke(noteModel: NoteModel) = repository.addNote(noteModel)
+    suspend operator fun invoke(noteModel: NoteModel) = run {
+        if (noteModel.noteTitle.isNotEmpty() && noteModel.note.isNotEmpty()) {
+            repository.addNote(noteModel)
+        } else {
+            Error("Please check title and note areas.")
+        }
+    }
 
 }
