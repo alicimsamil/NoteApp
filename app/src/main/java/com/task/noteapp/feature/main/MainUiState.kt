6ff8021959772752442bb16d38f5c7b71d959676
Ali Şamil Küçük <alicimsamil@gmail.com>
2023-05-07 23:28:34 +0300

    create main activity view model

---
+++ app/src/main/java/com/task/noteapp/feature/main/MainUiState.kt
@@ -0,0 +1,5 @@
+package com.task.noteapp.feature.main
+
+import com.task.noteapp.core.ui.UiState
+
+data class MainUiState(val splashAnimationState: Boolean = true) : UiState()
---
+++ app/src/main/java/com/task/noteapp/feature/main/MainViewModel.kt
@@ -0,0 +1,28 @@
+package com.task.noteapp.feature.main
+
+import androidx.lifecycle.viewModelScope
+import com.task.noteapp.core.ui.BaseViewModel
+import com.task.noteapp.core.ui.UiEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import kotlin.time.Duration.Companion.milliseconds
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : BaseViewModel() {
+
+    init {
+        _uiState.value = MainUiState()
+        getSplashAnimationState()
+    }
+
+    override fun onEvent(event: UiEvent) {}
+
+    private fun getSplashAnimationState() {
+        viewModelScope.launch {
+            delay(750.milliseconds)
+            _uiState.emit(MainUiState(splashAnimationState = false))
+        }
+    }
+}
