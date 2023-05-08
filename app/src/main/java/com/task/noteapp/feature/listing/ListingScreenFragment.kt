39b565fd344d91c1ee1bad66fe375f570d015c95
Ali Şamil Küçük <alicimsamil@gmail.com>
2023-05-08 22:49:59 +0300

    implement callback functions

--- app/src/main/java/com/task/noteapp/feature/listing/ListingScreenFragment.kt
+++ app/src/main/java/com/task/noteapp/feature/listing/ListingScreenFragment.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.task.noteapp.core.ui.BaseFragment
+import com.task.noteapp.core.ui.custom.CallBack
+import com.task.noteapp.core.ui.custom.DeleteNoteDialog
+import com.task.noteapp.data.model.NoteModel
 import com.task.noteapp.databinding.FragmentListingScreenBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -12,11 +15,11 @@ import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ListingScreenFragment :
-    BaseFragment<FragmentListingScreenBinding, ListingScreenViewModel>(FragmentListingScreenBinding::inflate) {
+    BaseFragment<FragmentListingScreenBinding, ListingScreenViewModel>(FragmentListingScreenBinding::inflate), OnClickListener {
 
     override val viewModel by viewModels<ListingScreenViewModel>()
 
-    private val pagingAdapter = NotesAdapter(NotesAdapter.NoteComparator)
+    private val pagingAdapter = NotesAdapter(NotesAdapter.NoteComparator, this)
 
     override var state = ListingUiState()
 
@@ -35,16 +38,29 @@ class ListingScreenFragment :
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.uiState.collectLatest {
                     state = it as ListingUiState
-
                     handleFailure()
                     handleLoading()
-
                     state.data?.collectLatest {
                         pagingAdapter.submitData(it)
                     }
                 }
             }
         }
+    }
+
+    override fun delete(noteModel: NoteModel) {
+        DeleteNoteDialog(object : CallBack{
+            override fun delete() {
+                viewModel.onEvent(ListingUiEvents.DeleteNote(noteModel))
+            }
+
+        }).show(parentFragmentManager,"DeleteNoteDialog")
+    }
+
+    override fun update(noteModel: NoteModel) {
+    }
+
+    override fun goDetail(id: Int?) {
     }
 
 }
